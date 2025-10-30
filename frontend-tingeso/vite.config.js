49eeb0ba29cb78b07dd8ca44839eb6fba7630ca2
DiegoRaAr <diego.ramos.a@usach.cl>
2025-10-30 01:18:37 -0300

    Dokerizacion completa

--- frontend-tingeso/vite.config.js
+++ frontend-tingeso/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Local 
+  /* Local 
   server: {
     proxy: {
       '/api': {
@@ -14,7 +14,8 @@ export default defineConfig({
       },
     },
   },
-  // FIN LOCAL
+  
+  // FIN LOCAL */
 })
 
 
