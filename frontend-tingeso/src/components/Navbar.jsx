d29da1bbacf40f6477e496596aef8698b975e1fa
DiegoRaAr <diego.ramos.a@usach.cl>
2025-11-30 01:38:35 -0300

    Cambio titulo

--- frontend-tingeso/src/components/Navbar.jsx
+++ frontend-tingeso/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-navbar fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Sistema de prestamo de herramientas Cambio prueba</a>
+        <a className="navbar-brand" href="#">Sistema de prestamo de herramientas</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
