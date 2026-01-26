8d94629833b5a72b51d1aeb469d1df6074e56f6a
DiegoRaAr <diego.ramos.a@usach.cl>
2026-01-26 12:32:17 -0300

    fill compose

--- frontend-tingeso/src/services/keycloak.js
+++ frontend-tingeso/src/services/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:8080",
-  realm: "loan-realm",
-  clientId: "loan-spa",
+  url: "http://localhost:70/auth",
+  realm: "tingeso-realm",
+  clientId: "frontend-app",
 }); 
 
 export default keycloak;
--- init-mysql/create-keycloak-db.sql
+++ init-mysql/create-keycloak-db.sql
@@ -1,3 +1,7 @@
+-- Crear la base de datos para Keycloak
 CREATE DATABASE IF NOT EXISTS keycloak_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON keycloak_db.* TO 'diego'@'%' IDENTIFIED BY 'diego1234';
+
+-- En MySQL 8.0+, el usuario 'diego' ya se crea via MYSQL_USER en docker-compose
+-- Solo necesitamos darle permisos sobre keycloak_db
+GRANT ALL PRIVILEGES ON keycloak_db.* TO 'diego'@'%';
 FLUSH PRIVILEGES;
