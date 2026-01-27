994c5e082ad6880e721ff1620ea28b822187c783
DiegoRaAr <diego.ramos.a@usach.cl>
2026-01-26 23:42:32 -0300

    Fix: Update Keycloak URL to AWS IP

--- frontend-tingeso/src/services/keycloak.js
+++ frontend-tingeso/src/services/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:70/auth",
+  url: "http://15.228.56.166:70/auth",
   realm: "tingeso-realm",
   clientId: "frontend-app",
 }); 
